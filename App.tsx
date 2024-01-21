b4bf39a75355e2c4b05b1c7b9864df5b179ab6a4
williamjayjay <williamdn@yahoo.com>
2024-01-21 19:05:09 -0300

    feat: add routes

--- App.tsx
+++ App.tsx
@@ -3,8 +3,7 @@ import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from '@theme/index'
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
 
   );
