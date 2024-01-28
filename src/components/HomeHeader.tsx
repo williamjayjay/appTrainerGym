c0fea4a8ea6cb92141654f4087971235b65c7872
williamjayjay <williamdn@yahoo.com>
2024-01-28 20:10:07 -0300

    feat: add component screenHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -5,7 +5,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 
 export function HomeHeader() {
     return (
-        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+        <HStack bg="gray.600" pt={12} pb={5} px={8} alignItems="center">
             <UserPhoto
                 source={{ uri: 'https://github.com/williamjayjay.png' }}
                 size={16}
